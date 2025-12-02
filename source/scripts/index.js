d384ee768c5f71500036f2b4e17acef0e3aded94
Aleksandr_Volkov <AVolkov211@yandex.ru>
2025-12-02 23:04:03 +0300

    сохран-2

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,28 @@
-const headerToggle = document.querySelector('.navigation__toggle');
+const navElement = document.querySelector('.navigation');
+if (navElement) {
+  const navButton = navElement.querySelector('.navigation__toggle');
+
+  navElement.classList.add('navigation--close');
+  navElement.classList.remove('navigation--nojs');
+
+  navButton.addEventListener('click', () => {
+
+    if (navElement.classList.contains('navigation--close')) {
+      navElement.classList.remove('navigation--close');
+      navElement.classList.add('navigation--open');
+
+      console.log('Привет');
+
+    } else {
+      navElement.classList.remove('navigation--open');
+      navElement.classList.add('navigation--close');
+    }
+  });
+}
+
+
+
+/* const headerToggle = document.querySelector('.navigation__toggle');
 const navList = document.querySelector('.navigation__list');
 
 if (headerToggle && navList) {
@@ -6,5 +30,5 @@ if (headerToggle && navList) {
     headerToggle.classList.toggle('navigation__toggle--active');
     navList.classList.toggle('navigation__list--close');
   });
-}
+}*/
 /* в этот файл добавляет скрипты*/
